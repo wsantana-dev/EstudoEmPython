0afb149e0307ccc97cd3975f56b3569d46c8ea21
Weslley Santana <weslleysantana2oo2@outlook.com>
2025-09-02 21:29:55 -0300

    condições aninhadas

--- ex001.py
+++
@@ -1,8 +0,0 @@
-num = int(input('digite um numero: '))
-
-if num % 2 == 0:
-    print(f'o numero {num} é par')
-else:
-    print(f'o numero {num} é impar')
-
-    #primeiro exercício classico apenas para testar um commit
