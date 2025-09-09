19dbd79bef63699c3fba42b14c5c7bee8bc2166d
Weslley Santana <weslleysantana2oo2@outlook.com>
2025-09-08 21:16:10 -0300

    Update ex004.py

--- condições aninhadas/ex004.py
+++ condições aninhadas/ex004.py
@@ -2,3 +2,21 @@
 #o primeiro valor é maior
 #o segundo valor é maior
 #não existe valor maior, os dois são iguais
+
+print('-=' * 20)
+print('VAMOS VER QUAL VALOR É MENOR OU MAIOR')
+print('-=' * 20)
+
+firstNumber = int(input('digite o primeiro valor: '))
+secondNumber = int(input('digite o segundo valor: '))
+
+firstmax = max(firstNumber, secondNumber)
+
+
+
+if firstNumber > secondNumber or firstNumber < secondNumber:
+    print(f'O numero maior entre os dois é {firstmax}')
+elif firstNumber == secondNumber:
+    print('os dois são igual')
+
+
