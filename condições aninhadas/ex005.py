3169a8fcbd3908d7487420cfff6252ac94f24c7e
Weslley Santana <weslleysantana2oo2@outlook.com>
2025-09-08 21:27:49 -0300

    atividade de alistamento

--- condições aninhadas/ex005.py
+++ condições aninhadas/ex005.py
@@ -2,4 +2,17 @@
 #se ele ainda vai se alistar ao serviço militar
 #se é a hora de se alistar
 #se já passou do tempo de alistamento
-#seu programa também dvera mostrar o tempo que falta ou que passou do prazo
+#seu programa também devera mostrar o tempo que falta ou que passou do prazo
+
+age = int(input('qual sua idade? '))
+
+if age < 18:
+    print('você pode se alistar somente aos 18 anos')
+    print('ainda te falta {} anos'.format((18 - age)))
+elif age == 18:
+    print('você ja pode se alistar')
+else:
+    print('voce ja passou do prazo de alistamento')
+    print('voce ja passou {} anos'.format((age - 18)))
+
+
