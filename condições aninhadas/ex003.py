7386a5ce97f99dd62c1a4a50231367b0a1353e0b
Weslley Santana <weslleysantana2oo2@outlook.com>
2025-09-18 21:07:38 -0300

    DUAS ATIVIDADES

--- condições aninhadas/ex003.py
+++ condições aninhadas/ex003.py
@@ -1,3 +1,18 @@
 #escreva um programa que leia um número inteiro qualquer e peça
 #para o usuário escolher qual a base de conversão
 #binário, octal ou hexadecimal
+
+num = int(input('digite um numero inteiro: '))
+print('''Escolha uma das bases para conversão:
+[ 1 ] converter para BINÁRIO
+[ 2 ] converter para OCTAL
+[ 3 ] converter para HEXADECIMAL''')
+opcao = int(input('sua opção: '))
+if opcao == 1:
+     print('{} convertido para BINÁRIO é igual a {}'.format(num, bin(num)[2:]))
+elif opcao == 2:
+    print('{} convertido para OCTAL é igual a {}'.format(num, oct(num)[2:]))
+elif opcao == 3:
+    print('{} convertido para HEXADECIMAL é igual a {}'.format(num, hex(num)[2:]))
+else:
+    print('Opcão invalida. Tente novamente.')
--- condições aninhadas/ex008.py
+++ condições aninhadas/ex008.py
@@ -2,3 +2,19 @@
 #e se é Equilátero: todos lados iguais
 #se é Isósceles: dois lados igual
 #Escaleno: todos os lados diferentes
+
+r1 = float(input('Primeiro segmento: '))
+r2 = float(input('Segundo segmento: '))
+r3 = float(input('Terceiro segmento: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('os segmentos acima podem formar um triangulo', end='')
+    if r1 == r2 == r3:
+        print('EQUILATERO')
+    elif r1 != r2 != r3 != r1:
+        print('ESCALENO')
+    else:
+        print('ISÓSCELES')
+
+else:
+    print('os segmentos acima NÃO podem formar um triangulo')
+
